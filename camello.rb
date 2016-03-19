1ab22a6799ef261fb2bad89daceb043dc89c1fd9
Bastián Mavrakis Córdova <bamavrakis@uc.cl>
2016-03-19 13:28:33 -0300

    Yadrans y agregué los archivos de prueba

--- camello.rb
+++ camello.rb
@@ -3,12 +3,12 @@ module CarreraCamellos
 	class Camello
 
 		attr_accessor :name, :id, :puntaje, :tiempo, :lugar
-		attr_reader :cameyardas
+		attr_reader :cameyadrans
 
 		def initialize()
 			@nombre = ""
 			@id = ""
-			@cameyardas = [] #arreglo yardas/s <- cada elemento es un segundo
+			@cameyadrans = [] #arreglo yadrans/s <- cada elemento es un segundo
 			@tiempos_xvueltas = [] #arreglo de 5
 			@puntaje = 0
 			@tiempo = 0
